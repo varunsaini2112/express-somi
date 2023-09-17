5158fd6660744fc0c358303fa640ec0fb4cc10a2
varunsaini21 <varun.saini@squarehealth.com>
2023-09-17 15:32:50 +0530

    index file updated with mongoose connection

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-	console.log("Got here /");
-
-	res.status(200).send("Hi");
-});
-
 const todoRoutes = require("./routes/todo");
+const authRoutes = require("./routes/auth");
 
 app.use("/todo", todoRoutes);
+app.use("/auth", authRoutes);
 
-app.listen(3000);
+mongoose
+	.connect(process.env.MONGODB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then(() => console.log("mongoose connection successful"))
+	.catch(() => console.log("mongoose connection failed"));
+
+app.listen(PORT, () => {
+	console.log("Server Listening on PORT:", PORT);
+});
