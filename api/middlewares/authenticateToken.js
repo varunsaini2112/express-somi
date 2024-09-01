cd19d0f2ce05feef37a1f17a2532d4d49583d22c
varunsaini21 <varun.saini@squarehealth.com>
2024-09-01 23:16:25 +0530

    authentication of token moved from logout.js to separate file

---
+++ api/middlewares/authenticateToken.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+const Users = require("../schema/userSchema");
+const errorHandler = require("../utils/errorHandler");
+
+async function authenticateToken(req, res, next) {
+	try {
+		const authHeader = req.headers["authorization"] ?? "";
+		const accessToken = authHeader.split(" ")[1];
+
+		if (!accessToken) {
+			console.log("No token received");
+			res.status(401).send("Unauthorised access");
+			return;
+		}
+
+		const { email } = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+		const userDetails = await Users.findOne({ email });
+
+		if (!userDetails) {
+			console.log("User not found");
+			res.status(401).send("Unauthorised access");
+			return;
+		}
+
+        req.user = userDetails;
+		req.accessToken = accessToken;
+        next();
+	} catch (error) {
+		errorHandler(res)(error);
+	}
+}
+
+module.exports = authenticateToken;
