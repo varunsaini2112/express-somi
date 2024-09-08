1736711a3d78eb8287a96b8351968d636be45f88
varunsaini21 <varun.saini@squarehealth.com>
2024-09-09 01:36:04 +0530

    getList error handling added

--- api/controllers/todo/getList.js
+++ api/controllers/todo/getList.js
@@ -1,17 +1,22 @@
 const TodoList = require("../../schema/todoSchema");
+const errorHandler = require("../../utils/errorHandler");
 
 async function getList(req, res) {
-	const { _id } = req.user;
-	const todoList = await TodoList.findById(_id);
+	try {
+		const { _id } = req.user;
+		const todoList = await TodoList.findById(_id);
 
-	if (!todoList) {
-		console.log("No List available");
-		res.status(200).json({});
-		return;
+		if (!todoList) {
+			console.log("No List available");
+			res.status(200).json({});
+			return;
+		}
+
+		console.log("TodoList sent successfully");
+		res.status(200).json(todoList.todos);
+	} catch (error) {
+		errorHandler(res)(error);
 	}
-
-	console.log("TodoList sent successfully");
-	res.status(200).json(todoList.todos);
 }
 
 module.exports = getList;
